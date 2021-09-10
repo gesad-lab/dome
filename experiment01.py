7cc124d4540edf04c64098d2ff4196e6860d3eb8
Anderson Martins <andersonmg@gmail.com>
2021-09-10 14:05:16 -0300

    deleting the old gen files

--- experiment01.py
+++ experiment01.py
@@ -1,6 +1,7 @@
 from baseclasses import *
 import time
 import os
+import shutil
 
 userTest = None
 currentEntity = None
@@ -12,8 +13,17 @@ def boot():
     userTest = User(os.environ['DJANGO_SUPERUSER_USERNAME'], os.environ['DJANGO_SUPERUSER_PASSWORD'])
     currentEntity = userTest.MUP.currentEntity #only one entity in this version
 
+#deleting the old gen files
+root_path = os.path.dirname(__file__)
+print(root_path)
+for dir in os.listdir(root_path):
+    full_dir_path = os.path.join(root_path, dir)
+    if os.path.isdir(full_dir_path) and (dir != '.git'): 
+        shutil.rmtree(full_dir_path)
+    
 boot()
 i = 1
+
 while True:
     print(userTest.MUP.addAttribute(currentEntity, 'att_'+str(i), 'str'))
     time.sleep(30) # Sleep for some seconds
