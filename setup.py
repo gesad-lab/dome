7873d2c4ec2fb1c289ec1e3ae62597e28e2654bd
Anderson Martins <andersonmg@gmail.com>
2021-09-30 13:55:02 -0300

    v 0.0.8

--- setup.py
+++ setup.py
@@ -5,32 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="text2system",                     # This is the name of the package
-    version="0.0.5",                        # The initial release version
+    version="0.0.8",                        # The initial release version
     author="Anderson Martins Gomes",                     # Full name of the author
     author_email='andersonmg@gmail.com',
     url='https://github.com/andersonmgomes/text2system',
     description="A self-adaptative architecture implementation.",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(where="src"),   # List of all python modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],                                      # Information to filter the project on PyPi website
     python_requires='>=3.6',                # Minimum version requirement of the package
-    py_modules=['aiengine'
-                ,'analyticsengine'
-                ,'autonomouscontroller'
-                ,'businessprocessengine'
-                ,'domainengine'
-                ,'externalservice'
-                ,'integrationengine'
-                ,'interfacecontroller'
-                ,'multchannelapp'
-                ,'user'
-                , 'securityengine'],             # Name of the python package
-    
     package_dir={'':'src'},     # Directory of the source code of the package
-    install_requires=[]                     # Install other dependencies if any
+    packages=["text2system"],
 )
