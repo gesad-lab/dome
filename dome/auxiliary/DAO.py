7abe86f18616e29524077d995517e3d905af3966
Anderson Martins <andersonmg@gmail.com>
2022-12-16 15:20:22 -0300

    encapsulating the database functions in the DAO abstract class

---
+++ dome/auxiliary/DAO.py
@@ -0,0 +1,38 @@
+import os
+import sqlite3
+from abc import abstractmethod
+
+
+class DAO:
+    # static variable that keep database connection
+    __DB_CONNECTION = None
+    # get database file path
+    __DB_PATH_DIR = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'databases/')
+
+    def __init__(self):
+        # execute pragma foreign_keys = 1 to enable foreign keys
+        self.__execute_query("PRAGMA foreign_keys = 1")
+
+    # abstract method to get database file name
+    @abstractmethod
+    def get_db_file_name(self) -> str:
+        raise NotImplementedError("This method must be implemented.")
+
+    # initialize database connection with sqlite file "database/sdb.sqlite"
+    def __get_db_connection(self):
+        if DAO.__DB_CONNECTION is None:
+            DAO.__DB_CONNECTION = sqlite3.connect(DAO.__DB_PATH_DIR + self.get_db_file_name())
+            DAO.__DB_CONNECTION.row_factory = sqlite3.Row
+        # else: already initialized
+        return DAO.__DB_CONNECTION
+
+    # static method to execute a query using the database connection
+    def _execute_query(self, query, args=()):
+        conn = self.__get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, args)
+        conn.commit()
+        return cursor
+
+    def __del__(self):
+        self.__DB_CONNECTION.close()
--- dome/securityengine.py
+++ dome/securityengine.py
@@ -4,14 +4,11 @@ import sqlite3
 import json
 
 from dome.autonomouscontroller import AutonomousController
+from dome.auxiliary.DAO import DAO
 from dome.integrationengine import IntegrationEngine
 
 
-class SecurityEngine:
-    # static variable that keep database connection
-    __DB_CONNECTION = None
-    # get database file path
-    __DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'databases/sdb.sqlite')
+class SecurityEngine(DAO):
 
     def __init__(self, MUP, IE=None):
         self.__MUP = MUP  # Multichannel App
@@ -26,6 +23,9 @@ class SecurityEngine:
         # user access allowed
         self.__AC = AutonomousController(self)  # autonomous controller instance
 
+    def get_db_file_name(self) -> str:
+        return "sdb.sqlite"
+
     def getAC(self):
         return self.__AC
 
@@ -39,26 +39,8 @@ class SecurityEngine:
         # call Autonomous Controller
         return self.__AC.plan(opr, data)
 
-    # initialize database connection with sqlite file "database/sdb.sqlite"
-    @staticmethod
-    def __get_db_connection():
-        if SecurityEngine.__DB_CONNECTION is None:
-            SecurityEngine.__DB_CONNECTION = sqlite3.connect(SecurityEngine.__DB_PATH)
-            SecurityEngine.__DB_CONNECTION.row_factory = sqlite3.Row
-        # else: already initialized
-        return SecurityEngine.__DB_CONNECTION
-
-    # static method to execute a query using the database connection
-    @staticmethod
-    def __execute_query(query, args=()):
-        conn = SecurityEngine.__get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute(query, args)
-        conn.commit()
-        return cursor
-
     def get_user_by_chat_id(self, chat_id):
-        query_result = self.__execute_query("SELECT * FROM users WHERE chat_id = ?", (chat_id,)).fetchone()
+        query_result = self._execute_query("SELECT * FROM users WHERE chat_id = ?", (chat_id,)).fetchone()
         if query_result is None:
             return None
         # else
@@ -68,14 +50,14 @@ class SecurityEngine:
     def create_or_get_user(self, chat_id):
         query_result = self.get_user_by_chat_id(chat_id)
         if query_result is None:
-            self.__execute_query("INSERT INTO users(chat_id) VALUES (?)", (chat_id,))
+            self._execute_query("INSERT INTO users(chat_id) VALUES (?)", (chat_id,))
             query_result = self.get_user_by_chat_id(chat_id)
         return query_result
 
     def save_msg_handle_log(self, msg, user_id, response_obj):
         # transform the response_obj in a json string
         response_obj_json = json.dumps(response_obj, default=str)
-        self.__execute_query("INSERT INTO msg_handle_log(msg, user_id, response) VALUES (?, ?, ?)",
+        self._execute_query("INSERT INTO msg_handle_log(msg, user_id, response) VALUES (?, ?, ?)",
                              (msg, user_id, response_obj_json))
 
     def getAC(self):
