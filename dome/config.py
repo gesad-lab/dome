e6f82bf2b7ed6598a9896d7c43878d530ec7f5a1
Anderson Martins <andersonmg@gmail.com>
2023-01-21 05:39:51 -0300

    improvements in user messages

--- dome/config.py
+++ dome/config.py
@@ -24,17 +24,14 @@ WHERE_CLAUSE_WORDS = {'where', 'which', 'that', 'whose', 'whom', 'who', 'what', 
 
 # bot msgs
 MISUNDERSTANDING = [
-    """Um. I don't recognize it. Which operation do you want to do? Add, update, delete or get some information? 
-    (say 'help' for samples) """
-    ,
-    """Sorry, but I didn't get it. Try something like 'register new student with name 'Joseph'' or 
-    'get all students with gender=Male'\n(say 'help' for more samples)"""
-    , "Please, repeat in another way, because I didn't get it. (say 'help' for more info)"]
+    "Um. I don't recognize it. Which operation do you want to do? Add, update, delete or get some information?\n(say 'help' for samples)",
+    "Sorry, but I didn't get it. Try something like <i>'register new student with name 'Joseph''</i> or <i>'get all students with gender=Male'</i>\n(say 'help' for more samples)",
+    "Please, repeat in another way, because I didn't get it. (say 'help' for more info)"
+]
 
-GREETINGS = ["Hi! You can say something like 'Add student with name=Anderson'",
-             "Hello! Please say something like 'Get student with name=Anderson'",
-             "Hello! Good see you here! Please say some data operation like 'Include student with name=Anderson, "
-             "email=andersonmg@gmail.com' "
+GREETINGS = ["Hi! You can say something like <i>'Add student with name=Anderson'</i>",
+             "Hello! Please say something like <i>'Get student with name=Anderson'</i>",
+             "Hello! Good see you here! Please say some data operation like <i>'Include student with name=Anderson, email=andersonmg@gmail.com'</i>"
              ]
 
 BYE = ["Ok! Thank you. See you next time!",
@@ -42,16 +39,9 @@ BYE = ["Ok! Thank you. See you next time!",
        "Thank you! If you need add some info, please text me."
        ]
 
-HELP = ["""I'm a bot that helps you add your information in an organized, secure, and flexible way. Say what you 
-want to add, update, delete or only get info. \nFor example, say something like 'add a class with 
-name=Self-Adaptive Systems', 'view classes', or 'delete class name=Java'. """,
-        """I'm a bot that allows you to add your information using natural language. Like a traditional system, 
-    but more accessible and flexible. \nFor instance, to register a student, say 'add student with gender=Female, 
-    name=Mary, email=mary@school.com' or 'delete student name=Mary'. """,
-        """I'm your bot that securely saves your information. I understand better direct sentences. \nThus let me know 
-    first what you want to do (add, read or delete some data), what type the information you want to operate (a 
-    student, a class, a class registration, etc.), and, finally, the data itself. \nSome examples:\n'add a teacher 
-    with name='Paulo Henrique', gender='Male'\n'delete a student with name=Anderson'\n'get the class with name=Java'"""
+HELP = ["I'm a bot that helps you add your information in an organized, secure, and flexible way. Say what you want to add, update, delete or only get info. \nFor example, say something like <i>'add a class with name=Self-Adaptive Systems'</i>, <i>'view classes'</i>, or <i>'delete class name=Java'</i>.",
+        "I'm a bot that allows you to add your information using natural language. Like a traditional system, but more accessible and flexible.\nFor instance, to register a student, say <i>'add student with gender=Female, name=Mary, email=mary@school.com'</i> or <i>'delete student name=Mary'</i>.",
+        "I'm your bot that securely saves your information. I understand better direct sentences.\nThus let me know first what you want to do (add, read or delete some data), what type the information you want to operate (a student, a class, a class registration, etc.), and, finally, the data itself. \nSome examples:\n'<i>add a teacher with name='Paulo Henrique', gender='Male'\n'delete a student with name=Anderson'\n'get the class with name=Python'</i>"
         ]
 
 CANCEL = ['No problem! The operation was canceled successfully.']
@@ -61,22 +51,17 @@ CANCEL_WITHOUT_PENDING_INTENT = ['There is no operation to cancel.']
 CONFIRMATION_WITHOUT_PENDING_INTENT = ['There is no operation to confirm.']
 
 ASK_CONFIRM = ['OK to confirm current operation;\nCANCEL to cancel. ;)]',
-               "[Any time you can say 'ok' to confirm the operation, or 'cancel' to cancel the current operation]"
-               ]
+               "[Any time you can say 'ok' to confirm the operation, or 'cancel' to cancel the current operation]"]
 
 ATTRIBUTE_FORMAT = [
-    "I'll understand better if the data is in the following format:\n 'data_name = data_value'.\nObserve the "
-    "examples:"
-    "\n- <i>add student with name = 'Anderson', and age = 21</i>"
-    "\n- <i>update the student with name = 'Anderson', setting the age = 21</i>"
-    "\n- <i>delete the student with name = 'Anderson'</i>"
-    "\n- <i>get the student with name = 'Anderson'</i>"]
+    "I'll understand better if the data is in the following format:\n 'data_name = data_value'.\nObserve the examples:\n- <i>add student with name = 'Anderson', and age = 21</i>\n- <i>update the student with name = 'Anderson', setting the age = 21</i>\n- <i>delete the student with name = 'Anderson'</i>\n- <i>get the student with name = 'Anderson'</i>"
+]
 
 ATTRIBUTE_OK = lambda opr, clas, att, where: [f"Ok! I got it!\nWe are going to <b>{opr}</b> a <b>"
                                               f"{clas.replace('_', ' ').title()}</b>."
                                               + (f"\nWith attributes = <b>{att}</b>." if att else "")
                                               + (f"\nOnly when <b>{where}</b>." if where else "")
-                                              + f"\nSay 'Ok' to confirm\n'Cancel' to"
+                                              + f"\nSay <b>'OK'</b> to confirm or <b>'CANCEL'</b> to"
                                               f" cancel this operation."]
 
 SAVE_SUCCESS = ['Ok! Information saved successfully!',
@@ -101,14 +86,11 @@ LIMIT_REGISTERS_MSG = "A maximum of <b>" + str(LIMIT_REGISTERS) + \
 CLASS_NOT_IN_DOMAIN = lambda clas: [f"There is no information about <b>'{clas}'</b> saved. \nPlease, try something else."]
 
 MISSING_CLASS = [
-    "Would you please inform me of the information type that you want to operate? For instance, if you are trying to "
-    "add data about a student, say 'add student with name=\'Anderson\''."
-    "\n(If you wish to cancel this operation, say 'cancel'.)"]
+    "Would you please inform me of the information type that you want to operate? For instance, if you are trying to add data about a student, say <i>'add student with name=\'Anderson\''</i>.\n(If you wish to cancel this operation, say '<b>CANCEL</b>'.)"]
 
 MULTIPLE_CLASSES = ['Please, try to inform the information type with only one word, ok?']
 
-GENERAL_FAILURE = '[ERROR] Sorry, but I could not complete the operation because of an internal error. Please, ' \
-                  'try again using other words. '
+GENERAL_FAILURE = 'Sorry, but I could not complete the operation. Please, try again using other words.\n(say <b>HELP</b> for examples)'
 
 # config variables
 MANAGED_SYSTEM_NAME = 'managedsys'
